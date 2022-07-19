becf59a868cd9e5e4e15f89276904193a7498db3
Henry Munoz <alexcol123@yahoo.com>
2022-07-19 17:00:15 -0400

    backend  completed basic backen for Jobs App

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 require('express-async-errors')
 
 // Extra security packages
-// const helmet = require('helmet')
-// const cors = require('cors')
-// const xss = require('xss-clean')
-// const rateLimiter = require('express-rate-limit')
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
 
 const express = require('express')
 const app = express()
@@ -23,9 +23,19 @@ const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
 // Extra security
-
-// Middleware
+// code bellow is to be used when i use rate limiter , before i push to heroky
+// app.set('trust proxy', 1)
+// app.use(
+//   rateLimiter({
+//     windowMs: 15 * 60 * 1000, // 15 minutes
+//     max: 500,
+//   })
+// )
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
 
 // routes
 app.get('/', (req, res) => res.send('Hello World!'))
