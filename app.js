df8047152fd173f606f764685395cd545f0d04d9
Henry Munoz <alexcol123@yahoo.com>
2022-07-19 09:04:40 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+require('express-async-errors')
+
+// Extra security packages
+// const helmet = require('helmet')
+// const cors = require('cors')
+// const xss = require('xss-clean')
+// const rateLimiter = require('express-rate-limit')
+
+const express = require('express')
+const app = express()
+
+// ConnectDB
+const connectDB = require('./db/connect')
+//const authenticateUser = require('./middleware/authentication')
+
+// Routers
+
+// Error Handler
+
+// Extra security
+
+//  app.get('/', (req, res) => res.send('Hello World!'))
+
+const port = process.env.PORT || 5000
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`)
+    })
+  } catch (error) {
+    console.log('error')
+  }
+}
+
+start()
